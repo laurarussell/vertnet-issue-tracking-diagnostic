c60994537ae89aeea1491dde265ac2f6edf906e4
laurarussell <laura.anne.russell@gmail.com>
2015-04-29 14:40:26 -0500

    more edits to bundle for gh-pages

--- target/js/main.bundle.js
+++ target/js/main.bundle.js
@@ -59,9 +59,14 @@ var nodes = angular.module('dataset', []);
 
 nodes.controller('DatasetController', ['$http', function($http) {
   var self = this;
+  
+   var cdbAPI = "http://vertnet.cartodb.com/api/v2/sql?q=" +
+	   "SELECT icode,orgname,github_orgname,github_reponame,lastindexed,url,ipt" +
+	   "FROM resource " +
+	   "WHERE ipt=true AND networks like '%25VertNet%25' " + // encode % as %25
+	   "ORDER BY github_orgname,github_reponame";
 
-  var cdbAPI = 'http://vertnet.cartodb.com/api/v2/sql?q=SELECT%20icode,orgname,github_orgname,github_reponame,lastindexed,url,ipt%20FROM%20resource%20WHERE%20ipt=true%20AND%20networks%20LIKE%20%27%25VertNet%25%27';
-  var githubAPI ='https://api.github.com/search/issues?q=repo:';
+  var githubAPI ="https://api.github.com/search/issues?q=repo:";
 
   self.datasets = [];
 
@@ -71,12 +76,9 @@ nodes.controller('DatasetController', ['$http', function($http) {
     $http.get(cdbAPI).success(function (data) {
       self.datasets = data.rows;
 
-
-
       // load each GitHub count
       angular.forEach(self.datasets, function(dataset) {
-        console.log(dataset);
-        $http.get(githubAPI + dataset.github_orgname + '/ '+ dataset.github_reponame + '+is:issue+is:closed+-label:report').success(function (data) {
+        $http.get(githubAPI + dataset.github_orgname + "/"+ dataset.github_reponame + "+is:issue+is:closed+-label:report").success(function (data) {
           dataset.total_count = data;
         });
       });
