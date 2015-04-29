de0e1028dc30d1775cfd58add9b24970c66b93f6
laurarussell <laura.anne.russell@gmail.com>
2015-04-29 14:14:55 -0500

    changed bundle for gh-pages

--- target/js/main.bundle.js
+++ target/js/main.bundle.js
@@ -55,37 +55,37 @@
 	__webpack_require__(2);
 	__webpack_require__(3);
 	
-	var nodes = angular.module('dataset', []);
-	
-	nodes.controller('DatasetController', ['$http', function($http) {
-	  var self = this;
-	
-	  var cdbAPI = 'http://vertnet.cartodb.com/api/v2/sql?q=SELECT%20gbifdatasetid,title,lastindexed,url,count,ipt%20FROM%20resource';
-	  var gbifAPI ='http://api.gbif.org/v1/occurrence/count?datasetKey=';
-	
-	  self.datasets = [];
-	
-	  // Call the VN CartoDB API to get the list of VN datasets.
-	  var getDatasets = function() {
-	
-	    $http.get(cdbAPI).success(function (data) {
-	      self.datasets = data.rows;
-	
-	
-	
-	      // load each GBIF count
-	      angular.forEach(self.datasets, function(dataset) {
-	        console.log(dataset);
-	        $http.get(gbifAPI + dataset.gbifdatasetid).success(function (data) {
-	          dataset.gbifCount = data;
-	        });
-	      });
-	    });
-	  }
-	
-	  // call the function to populate the node list
-	  getDatasets();
-	}]);
+var nodes = angular.module('dataset', []);
+
+nodes.controller('DatasetController', ['$http', function($http) {
+  var self = this;
+
+  var cdbAPI = 'http://vertnet.cartodb.com/api/v2/sql?q=SELECT%20icode,orgname,github_orgname,github_reponame,lastindexed,url,ipt%20FROM%20resource%20WHERE%20ipt=true%20AND%20networks%20LIKE%20%27%25VertNet%25%27';
+  var githubAPI ='https://api.github.com/search/issues?q=repo:';
+
+  self.datasets = [];
+
+  // Call the VN CartoDB API to get the list of VN datasets.
+  var getDatasets = function() {
+
+    $http.get(cdbAPI).success(function (data) {
+      self.datasets = data.rows;
+
+
+
+      // load each GitHub count
+      angular.forEach(self.datasets, function(dataset) {
+        console.log(dataset);
+        $http.get(githubAPI + dataset.github_orgname + '/ '+ dataset.github_reponame + '+is:issue+is:closed+-label:report').success(function (data) {
+          dataset.total_count = data;
+        });
+      });
+    });
+  }
+
+  // call the function to populate the node list
+  getDatasets();
+}]);
 
 
 /***/ },
